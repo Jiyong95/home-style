1f29fe09c4664aa6b0a7401b915540ce0ce4b38b
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-04 01:39:18 +0900

    feat: API, Icon url 상수로 설정

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const API_URL = 'https://cdn.ggumim.co.kr/test/image_product_link.json';
 const SEARCH_ICON = '//cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png';
 const CLOSE_ICON = '//cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png';
+const ARROW_ICON = '//cdn.ggumim.co.kr/storage/20211102181936xqHzyWAmb8.png';
 
-export { API_URL, SEARCH_ICON, CLOSE_ICON };
+export { API_URL, SEARCH_ICON, CLOSE_ICON, ARROW_ICON };
